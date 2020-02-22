aa749aa08e3f120dd1745e19cc9d03e4bae5b5ce
danielmoreano1996 <61156450+danielmoreano1996@users.noreply.github.com>
2020-02-22 17:03:08 -0500

    Add files via upload

---
+++ JavaApplication2/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author damoreanope
+ */
+public class JavaApplication2 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        FrmPPTLS frmPPTLS = new FrmPPTLS();
+        
+        frmPPTLS.setVisible(true);
+    }
+    
+}
